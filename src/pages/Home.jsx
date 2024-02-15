6bae7ba3d2df2d76234c7ab06529cb5dcfea3cdf
sebastien Perrot <spholder4@gmail.com>
2024-02-15 22:55:47 +0100

    gestion message erreur

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,26 @@ import UseAPICall from "../hooks/UseAPICall";
 import CartLocation from "../components/CartLocation";
 import Banner from "../components/Banner";
 import bannerImg from "../assets/images/header-banner.jpeg";
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
   const data = UseAPICall();
-  console.log(data);
+  console.log(data); 
 
   return (
     <div>
       <Banner img={bannerImg} title={"Chez vous, partout et ailleurs"} />
 
       <section className="locationGallery">
-        {data.locationData &&
+        {data.locationData ? (
           data.locationData.map((obj, index) => (
-            <CartLocation key={index} title={obj.title} img={obj.cover} />
-          ))}
+            <NavLink key={index} to={`/card/${obj.id}`}>
+              <CartLocation title={obj.title} img={obj.cover} />
+            </NavLink>
+          ))
+        ) : (
+          <p className="error-msg">Erreur lors de la récupération des données</p>
+        )}
       </section>
     </div>
   );
