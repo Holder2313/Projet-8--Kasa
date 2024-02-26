b981735a5b5c16805d22cc681f8d8da55a313c9e
sebastien Perrot <spholder4@gmail.com>
2024-02-26 14:45:52 +0100

    gh page

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "Projet-8--Kasa",
   plugins: [react()],
   server: {
     proxy: {
